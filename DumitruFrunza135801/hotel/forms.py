5f933b0b7a670d4400e2d169a260c547951d1e8d
MRdima98 <62344480+MRdima98@users.noreply.github.com>
2022-07-17 20:00:52 +0200

    registra_struttura view quasi finita

--- DumitruFrunza135801/hotel/forms.py
+++ DumitruFrunza135801/hotel/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 
 class NameForm(forms.Form):
-    NomeAlbergo = forms.CharField(label='' , max_length =50, widget=forms.TextInput(attrs={'class': 'form-control base-height','placeholder':'Nome albergo'}))
-    IVA = forms.CharField(label='' , max_length =15, widget=forms.TextInput(attrs={'class': 'form-control base-height','placeholder':'IVA'}))
-    via = forms.CharField(label='' , max_length =50, widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Via'}))
-    stelle = forms.CharField(label='' , widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Quante stelle'}))
-    costoCamera = forms.CharField(label='' , widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Costo camera'}))
-    camereTot = forms.CharField(label='' , widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Camere totali'}))
-    piscina = forms.BooleanField(label='Opzioni')
-    
-    
+    NomeAlbergo = forms.CharField(label='Nome' , max_length =50, widget=forms.TextInput(attrs={'class': 'form-control base-height','placeholder':'Nome albergo'}), help_text='Yes')
+    IVA = forms.CharField(label=None , max_length =15, widget=forms.TextInput(attrs={'class': 'form-control base-height','placeholder':'IVA'}), help_text='Yes')
+    via = forms.CharField(label=None , max_length =50, widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Via'}), help_text='Yes')
+    stelle = forms.CharField(label=None , widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Quante stelle'}), help_text='Yes')
+    costoCamera = forms.CharField(label=None , widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Costo camera'}), help_text='Yes')
+    camereTot = forms.CharField(label=None , widget=forms.TextInput(attrs={'class': 'form-control base-height very-light-top-margin','placeholder':'Camere totali'}), help_text='Yes')
+    my_hidden = forms.CharField(widget=forms.HiddenInput())
+    piscina = forms.BooleanField(label='Piscina',label_suffix='')
+    idro = forms.BooleanField(label='Idro',label_suffix='')
+    palestra = forms.BooleanField(label='Palestra',label_suffix='')
+    descrizione = forms.CharField(label=None, widget=forms.Textarea(attrs={'class': 'form-control light-top-margin no-resize','placeholder':'Descrizione'}), help_text='No')
